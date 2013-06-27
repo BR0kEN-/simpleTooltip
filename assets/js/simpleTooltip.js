0f497cf280ae34c0d7f17f11864c5aba3c3b6c6e
Serg <support@firstvector.org>
2013-06-27 19:35:56 +0300

    Removal training comments in simpleTooltip.js

--- assets/js/simpleTooltip.js
+++ assets/js/simpleTooltip.js
@@ -1,11 +1,5 @@
-/*!
-	@Follow us on Twitter: https://twitter.com/firstvector
-	@Visit our website: http://firstvector.org/
-	@See information about our team: http://firstvector.org/humans.txt
-*/
-
 /**
-*	@maxWidth (int) максимальная ширина подсказки.
+*	@maxWidth (int) max width of tooltip.
 */
 (function(){
 	$.simpleTooltip = function(a){
@@ -21,20 +15,10 @@
 
 			if (e.type === 'mouseenter') {
 
-				/**
-				*	Умножим кол-во символов на среднюю ширину одного из них (Arial, 11px).
-				*/
 				var width = $(this).data('title').length * 5, txt;
 
-				/**
-				*	Если подсказка содержит много текста, то ее ширина обрезается до указанной в настройках и к тексту
-				*	применяется особое форматирование: выравнивание по левому краю и уменьшение высоты строки.
-				*/
 				if (width > maxWidth) width = maxWidth, txt = 'text-align:left;line-height:17px;padding:2px 5px;';
 
-				/**
-				*	Прибавим к ширине сумму боковых значений padding, разделим пополам и получим единицу смещения влево.
-				*/
 				head.append('<style id="simpleTooltip">[data-tooltip]:before{width:'+ width +'px;'+ txt +'}[data-tooltip$=th]:before{margin-left:-'+ ((width + 10) / 2) +'px}</style>');
 
 			} else $('#simpleTooltip').remove();
