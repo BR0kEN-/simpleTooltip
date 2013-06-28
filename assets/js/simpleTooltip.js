b86d8686e27a1483f7b00d15b4c3755139acdb63
Serg <support@firstvector.org>
2013-06-28 03:16:59 +0300

    Fixed critical bugs

--- assets/js/simpleTooltip.js
+++ assets/js/simpleTooltip.js
@@ -1,9 +1,15 @@
+/*!
+	Follow us on Twitter: https://twitter.com/firstvector
+	Visit our website: http://firstvector.org/
+	See information about our team: http://firstvector.org/humans.txt
+*/
+
 /**
-*	@maxWidth (int) max width of tooltip.
+*	@max (int) max width of tooltip.
 */
 (function(){
 	$.simpleTooltip = function(a){
-		var maxWidth = a ? a : 300, head = $('head');
+		var max = a ? a : 300, body = $('body');
 
 		$('[data-tooltip]').each(function(){
 
@@ -13,15 +19,19 @@
 
 		}).on('mouseenter mouseleave', function(e){
 
-			if (e.type === 'mouseenter') {
+			if (e.type == 'mouseenter') {
 
-				var width = $(this).data('title').length * 5, txt;
+				t = $(this);
 
-				if (width > maxWidth) width = maxWidth, txt = 'text-align:left;line-height:17px;padding:2px 5px;';
+				body.append('<div id="tooltip_width">'+ t.data('title') +'</div>');
 
-				head.append('<style id="simpleTooltip">[data-tooltip]:before{width:'+ width +'px;'+ txt +'}[data-tooltip$=th]:before{margin-left:-'+ ((width + 10) / 2) +'px}</style>');
+				width = $('#tooltip_width').width(), styles = '';
 
-			} else $('#simpleTooltip').remove();
+				if (width > max) width = max, styles = '[data-tooltip]:before{width:'+ width +'px;text-align:left;line-height:17px;padding:2px 5px;white-space:normal}';
+				if (t.data('tooltip').slice(-2) == 'th') styles += '[data-tooltip$=th]:before{margin-left:-'+ ((width + 10) / 2) +'px}';
+				if (styles) body.append('<style id="tooltip_style">'+ styles +'</style>');
+
+			} else $('#tooltip_style, #tooltip_width').remove();
 
 		});
 	};
