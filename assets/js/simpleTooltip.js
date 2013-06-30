fb69434bbdf46729d0863c00287cf122eea1ee21
Дмитрий Пацура <zaets28rus@gmail.com>
2013-06-30 08:02:27 +0400

    Some js code style refactoring

--- assets/js/simpleTooltip.js
+++ assets/js/simpleTooltip.js
@@ -10,33 +10,46 @@
 (function(){
 	$.simpleTooltip = function(a){
 		if (!/msie 8/.test(navigator.userAgent.toLowerCase())) {
-			var max = a ? a : 300, body = $('body');
+			var max = a ? a : 300,
+				body = $('body');
 
 			$('[data-tooltip]').each(function(){
+				var $t = $(this);
 
-				t = $(this);
+				$t.attr('data-title', this.title).removeAttr('title');
 
-				t.attr('data-title', this.title).removeAttr('title');
-				if (t.css('position') == 'static') t.css('position', 'relative');
+				if ($t.css('position') == 'static') {
+					$t.css('position', 'relative');
+				}
 
 			}).on('mouseenter mouseleave', function(e){
-
 				if (e.type == 'mouseenter') {
+					var $t = $(this);
 
-					t = $(this);
+					body.append('<div id="tooltip_width">'+ $t.data('title') +'</div>');
 
-					body.append('<div id="tooltip_width">'+ t.data('title') +'</div>');
+					var width = $('#tooltip_width').width(),
+						styles = '';
 
-					width = $('#tooltip_width').width(), styles = '';
+					if (width > max) {
+						width = max,
+						styles = '[data-tooltip]:before{width:'+ width +'px;text-align:left;line-height:17px;padding:2px 5px;white-space:normal}';
+					}
 
-					if (width > max) width = max, styles = '[data-tooltip]:before{width:'+ width +'px;text-align:left;line-height:17px;padding:2px 5px;white-space:normal}';
-					if (t.data('tooltip').slice(-2) == 'th') styles += '[data-tooltip$=th]:before{margin-left:-'+ ((width + 10) / 2) +'px}';
-					if (styles) body.append('<style id="tooltip_style">'+ styles +'</style>');
+					if ($t.data('tooltip').slice(-2) == 'th') {
+						styles += '[data-tooltip$=th]:before{margin-left:-'+ ((width + 10) / 2) +'px}';
+					}
 
-				} else $('#tooltip_style, #tooltip_width').remove();
-
+					if (styles) {
+						body.append('<style id="tooltip_style">'+ styles +'</style>');
+					}
+				} else {
+					$('#tooltip_style, #tooltip_width').remove();
+				}
 			});
-		} else return !1;
+		} else {
+			return !1;
+		}
 	};
 })(jQuery);
 
